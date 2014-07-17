f96792d670cc088ad5fc84ef09e6e27d68685bd9
sandygiuliani <sandraxgiuliani@gmail.com>
2014-07-17 17:58:11 +0100

    chembl clustering

--- drug_repo.py
+++ drug_repo.py
@@ -1500,7 +1500,7 @@ def run_smsd(query, target, flag, threshold, dic_map=None):
 
     #loop over drug in the map
     for drug in dic_map:
-      #logger.info(query)
+      logger.info(drug)
       # get the drug smiles
       drug_smi = query[drug]
       # list of cc that match each drug
@@ -1511,40 +1511,43 @@ def run_smsd(query, target, flag, threshold, dic_map=None):
       sim07 = []
       # each cc we have in the list
       for cc in dic_map[drug]:
-        # get the smiles
-        cc_smi = target[cc]
-        subprocess.call("sh SMSD.sh -Q SMI -q \"" + str(drug_smi) + \
-                      "\" -T SMI -t \"" + str(cc_smi) + "\" -r -m -z -b", 
-                      shell=True)
-        # get list from lines in molDescriptors output
-        mol_des = file_to_lines("molDescriptors.out")
-        #logger.info(mol_des)
-        # make string out of list
-        mol_str = ''.join(mol_des)
-        # logger.info(mol_str)
-        # tanimoto similarity string or other string
-        str_match = 'Tanimoto (Sim.)= '
-        # find the string
-        str_numb = mol_str.find(str_match)
-        sim_index = (str_numb + len(str_match))
-        # get similarity number and convert to float
-        similarity = float(mol_str[sim_index:(sim_index+3)])
+        logger.info(cc)
+        # check if cc is in the dic
+        if cc in target:
+          # get the smiles
+          cc_smi = target[cc]
+          subprocess.call("sh SMSD.sh -Q SMI -q \"" + str(drug_smi) + \
+                        "\" -T SMI -t \"" + str(cc_smi) + "\" -r -m -z -b", 
+                        shell=True)
+          # get list from lines in molDescriptors output
+          mol_des = file_to_lines("molDescriptors.out")
+          #logger.info(mol_des)
+          # make string out of list
+          mol_str = ''.join(mol_des)
+          # logger.info(mol_str)
+          # tanimoto similarity string or other string
+          str_match = 'Tanimoto (Sim.)= '
+          # find the string
+          str_numb = mol_str.find(str_match)
+          sim_index = (str_numb + len(str_match))
+          # get similarity number and convert to float
+          similarity = float(mol_str[sim_index:(sim_index+3)])
 
-        if similarity >= float(threshold):
-          # append to list
-          match_cc_list.append(cc)
+          if similarity >= float(threshold):
+            # append to list
+            match_cc_list.append(cc)
 
-        if similarity >= 1.0:
-          sim1.append(cc)
+          if similarity >= 1.0:
+            sim1.append(cc)
 
-        if similarity >= 0.9:
-          sim09.append(cc)
+          if similarity >= 0.9:
+            sim09.append(cc)
 
-        if similarity >= 0.8:
-          sim08.append(cc)
+          if similarity >= 0.8:
+            sim08.append(cc)
 
-        if similarity >= 0.7:
-          sim07.append(cc)
+          if similarity >= 0.7:
+            sim07.append(cc)
 
       # check the list is not empty
       if match_cc_list:
@@ -2235,10 +2238,10 @@ def main():
   #cc_smi_filt = {'11K': 'c1cc(ccc1CC(=O)Nc2cc([nH]n2)C3CC3)OCCN4CCCC4', '11U': 'c1cc(ccc1CNC(=O)C2CCCN2C(=O)CNC3CCCCC3)C(=N)N', '11P': 'c1cncc2c1CCC2C(O)(P(=O)(O)O)P(=O)(O)O', '11S': 'c1cc2c(cc[nH]2)cc1Cl', '11R': 'CN(C)c1cccc(c1)OCCCCCCCCCCCC2=CCN3C4=C5C(=CC=CN5[Ru]367(N8CCCCC8C9N6CCCC9)N1CCCCC1C1N7CCC=C1)C=CC24', '4ID': 'Cc1ccc(c(c1)C)S(=O)(=O)C2=CN=C(NC2=O)SCC(=O)Nc3ccccc3C(F)(F)F', '4IG': 'CCc1c(c(nc(n1)N)N)c2ccc3c(c2)N(C(=O)C(O3)c4cc(cc(c4)F)F)CCCOC', '11X': 'c1ccc(cc1)NCc2cccnc2', '2OH': 'CC(C)(c1ccc(cc1)O)c2ccc(cc2)O', '2OJ': 'CC(C)N1CCC(CC1)C(=O)Nc2c(cccc2OCc3cc(on3)c4ccc(s4)Cl)OCCCOC5C(C(C(C(O5)COC(=O)C)OC(=O)C)OC(=O)C)OC(=O)C', 'N7P': 'CC(=O)N1CCCC1C(=O)O', 'N7F': 'c1ccc(cc1)Cn2c3c(c(c2N4CCCC(C4)N)C#N)N=CN(C3=O)Cc5ccnc6c5cccc6', 'WI2': 'COc1cccc(c1)c2cnc(c(n2)N3CCC(CC3)C(=O)O)N', '2OP': 'CC(C(=O)O)O', 'N7O': 'CP(=O)(C(c1csc2c1cc(cc2)Cl)C(=O)NC=Cc3ccc(c(c3)F)F)O', 'ZZZ': 'C1C(NC2=C(N1)N=C(NC2=O)N)C=O', 'ZZY': 'c1ccc(c(c1)[N+](=O)[O-])S(=O)(=O)n2ccc3c2cc(cn3)C(=O)N', 'ZZT': 'Cc1ccc(c(c1)N)OC', 'ZZL': 'c1cc(c(c(c1)F)C2=NCc3cnc(nc3-c4c2cc(cc4)Cl)Nc5ccc(cc5)C(=O)O)F', 'ZZK': 'c1cc(c2c(c1)OCO2)c3cc(c(nc3)N)c4ccc(cc4)C(=O)N', 'ZZH': 'c1ccc(cc1)Cc2ccc(cc2)OC(Cc3ccccc3)C(=O)O', 'ZZG': 'Cc1cc(c(nc1C)c2ccccn2)Oc3ccnc(c3)Nc4cc(c(c(c4)OC)OC)OC', 'ZZF': 'Cc1ccc(c(n1)C)Oc2ccnc(c2)Nc3ccc(cc3)S(=O)(=O)N', 'ZZE': 'CCc1c(c(n(n1)CCO)CC)Oc2cc(cc(c2)C#N)C#N', 'ZZD': 'c1ccc(cc1)C(c2ccccc2)(c3ccccc3)SCC(C(=O)O)N', 'ZZA': 'c1ccc(cc1)n2cc(cn2)C(=O)O', 'ZZ7': 'CC1(C(NC(S1)C(C(=O)O)NC(=O)C(c2ccccc2)N)C(=O)O)C', 'ZZ6': 'CCNC(=O)c1cc2c(nc(nc2s1)N)c3ccc(cc3Cl)Cl', 'ZZ5': 'CCOc1ccc(cc1)c2c(c(nc3c2c(c(s3)C(=O)N)N)N)C#N', 'ZZ4': 'c1ccc(cc1)N=Nc2cnc(nc2c3ccc(cc3Cl)Cl)N', 'ZZ3': 'Cc1nc(nc(n1)SC)N', 'ZZ2': 'Cc1cc(nc(n1)N)OCCOC', 'ZZ0': 'c1cc(c(nc1)Nc2ccc(cc2)Cl)C(=O)O', 'N76': 'c1cc(cc(c1)S(=O)(=O)N)Nc2nc3c(c(n2)OCC4CCCCC4)nc[nH]3'}
   
   # # cc: create file with smiles to feed to openbabel
-  dic_to_txt(cc_smi_filt, 'test.smi')
+  #dic_to_txt(cc_smi_filt, 'test.smi')
 
   # # ligands converted to sdf
-  babel_smi_to_sdf('test.smi','test.sdf')
+  #babel_smi_to_sdf('test.smi','test.sdf')
 
 
 
@@ -2248,9 +2251,11 @@ def main():
   chembl_to_cc = merge_dic(chembl_dic,uniprot_filt, pdb_cc_dic)
   #logger.info(chembl_to_cc)
   #logger.info(pdb_cc_dic)
+  logger.info(chembl_to_cc['CHEMBL19'])
 
   #OVERWRITE CHEMBL_TO_CC FOR TESTING
-  #chembl_to_cc = {'CHEMBL997': ['SUF', '210', '1MV'],'CHEMBL877': ['AMH', '0GJ']}
+  #chembl_to_cc = {'CHEMBL870': ['SUF', '210', '1MV', 'NI9', 'RIS', '3N4', '3N5', '3N2', '3N3', '3N1', 'GO1', 'GO0', 'UNR', 'UNV', 'M0N', 'BFH', 'ZOL', 'BFQ', 'YL2', 'AHD', 'IPE', 'MS0', 'IPR', '4GA', 'RSX', '11P', 'YS4']}
+  #chembl_to_cc = {'CHEMBL870': ['SUF', '210', '1MV', 'NI9', 'RIS'],'CHEMBL19':['CEL', 'RYJ', 'ETS', '667', 'B30', 'BOS', 'OYS', 'BCN', 'OYQ', 'BOW', '2MZ', 'MIZ', '498', 'RYX', 'RYY', 'RYV', 'SRX', 'ZYX', 'Q4I', 'VZ4', '26E', 'EG2', 'TH7', 'SAB', 'BME', 'TH0', '0F3', 'SXS', 'SPM', 'A09', '4TR', 'IOF', 'IOE', 'WZA', 'IOC', 'PIU', 'IOA', 'SBR', 'TPD', 'BFG', 'I7B', 'BFE', 'ZEC', '2SD', 'FF3', '670', 'HAE', 'J45', '0NM', 'AYX', '0FZ', 'WZB', 'WZC', 'SBW', 'I7C', 'F2B', '1GD', 'FBU', 'PO1', 'EG3', 'BO1', 'EG1', 'B22', 'OY0', '1GO', 'O59', 'PHB', 'MZM', 'AG5', 'AG4', 'MZH', 'FFB', 'EVH', 'EVI', 'EVJ', '1MZ', 'BON', 'LC1', 'D8W', 'BR', 'EVD', 'EVE', 'EVF', '9FK', 'BEZ', '2VQ', 'E59', 'MG5', 'FUN', '1EZ', 'BEV', 'BEW', 'JDR', 'AL5', 'FLB', 'DHI', 'LSA', 'TUO', 'IT2', 'FSB', 'MNS', 'BL1', 'BL0', 'SBS', 'O48', 'MFS', 'E36', 'AMS', 'B19', 'PTS', 'TRI', '3BS', 'M28', 'M29', 'J90', 'M25', '3CC', 'E49', 'V21', '5UM', '5UN', 'AMJ', 'B09', 'P9B', 'SG6', 'TRU', 'DA4', 'WWV', 'FL1', 'HSM', 'INV', 'INW', 'INQ', '1CN', 'INL', 'INM', 'DT9', 'BE9', 'HTS', 'KCS', 'BE0', 'DT7', 'BE7', 'MPX', '2C7', 'FB2', 'BZU', 'SUA', 'TU0', 'COX', 'GLC', 'SMS', 'E38', 'B17', 'CTF', 'B15', 'AL8', 'AL9', 'AL6', 'AL7', 'AL4', 'IT5', 'AL2', 'AL3', 'RDT', 'AL1', 'O60', 'BEX', '84A', 'D2S', 'IE2', 'PMX', 'MB1', '0VY', '9TH', 'JKE', 'S6I', 'BSB', 'HSW', '5DS', 'AUC', 'E27', '4TZ', 'D9Z', '1SD', 'J71', 'J74', 'J75', 'GTQ', 'JS7', 'D9H', 'TE1', 'TE2', 'EVG', 'FBS', '1CR', 'CO2', 'FBW', 'FBV', 'BZ1', 'FBT', '0CR', 'ID4', 'ALE', 'SG5', 'F6B', 'E2I', 'SBB', '25X', 'MAJ', 'SU0', 'ARZ', 'MMC', 'AZM', '0VW', '0VV', 'TOR', '0VX', '0VZ', '1SA', 'J43', 'RCS', 'E90', 'RCO', 'AAS', 'NR2', 'DPN', 'WWZ', 'HQE', 'RYZ', 'GRE', 'MBO', '067', '25Y', 'E65', 'E50', 'RZ7', '03T', 'RZ5', 'D7A', 'P58', 'RZ1', 'RZ0', 'POF', 'FB1', 'I7A', 'VZ5', 'RZ8', 'STB', 'SG1', 'MTS', 'SG2', 'E02', 'SG4', 'SG7', 'FMS', 'KLT', 'SCN', 'S24', 'CNN', 'DWH', 'HGB', 'DA9', 'PPF', 'C1H', 'BHO', 'MS5', 'MS4', '1QV', 'EZL', 'D02', 'SDA', 'OSP', 'NKX', 'X0Q', 'E1E', 'DAW', 'R21', 'E1F']}
 
   # run smsd with drug to cc dic
 
@@ -2272,9 +2277,10 @@ def main():
   # rn clustering with 0.9 or other threshold
   # thresholds for similarity at 1, 0.9, 0.8, 0.7 are also written to output
   now = datetime.now()
-  chebl_cluster = run_or_pickle("7_chembl_cluster", run_smsd, 
+  chembl_cluster = run_or_pickle("7_chembl_cluster", run_smsd, 
                                 chembl_id_smi_opt, cc_smi_filt,
                                 "pair_2dic", 0.9, chembl_to_cc)
+  logger.info(chembl_cluster)
   then = datetime.now()
   tdelta = then - now
   logger.info(tdelta)
